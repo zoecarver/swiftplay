8aad5b0401f71a64f59404105011291b7a69f760
zoe <z.zoelec2@gmail.com>
2018-08-10 16:15:37 -0700

    Initial Commit

---
+++ swiftplay/swiftplay.h
@@ -0,0 +1,19 @@
+//
+//  swiftplay.h
+//  swiftplay
+//
+//  Created by Zoe IAMZOE.io on 8/10/18.
+//  Copyright © 2018 Zoe IAMZOE.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swiftplay.
+FOUNDATION_EXPORT double swiftplayVersionNumber;
+
+//! Project version string for swiftplay.
+FOUNDATION_EXPORT const unsigned char swiftplayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftplay/PublicHeader.h>
+
+
